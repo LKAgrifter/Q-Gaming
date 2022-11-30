bec775d5787de8877743803fd4b6cf548cce29df
JB2701 <jbailey7591@gmail.com>
2022-11-30 12:05:16 -0500

    while lopop

--- SDPDemo.m
+++ SDPDemo.m
@@ -9,14 +9,17 @@ BombGameScreen = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255])
 BombGamedisplay = [7,7,7,7;7,7,7,7]
 drawScene(BombGameScreen,BombGamedisplay)
 
-Game = Running;
+Game = LevelOne;
 
-while Game == Running
+while Game == LevelOne
     drawScene(BombGameScreen,BombGamedisplay)
     [x,y] = getMouseInput(BombGameScreen);
     if x == 2 && y == 1
         close all
         fprintf('Have a good day!\n')
         menurunning = false;
+    else
+        Game = LevelTwo;
     end
 end
+while Game == LevelTwo
