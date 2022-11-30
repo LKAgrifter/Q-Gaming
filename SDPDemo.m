4a6d46dc28868a0a5d951ec209318a6dbc039145
JB2701 <jbailey7591@gmail.com>
2022-11-30 12:27:19 -0500

    r

--- SDPDemo.m
+++ SDPDemo.m
@@ -5,21 +5,22 @@ close all
 %backround = simpleGameEngine('grid.png', 10, 10, 5, [161,130,45]);
 %board = [1,2,3,4,5; 1,2,3,4,5; 1,2,3,4,5; 1,2,3,4,5];
 %drawScene(backround,board)
-BombGameScreen = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255])
-BombGamedisplay = [7,7,7,7;7,7,7,7]
+BombGameScreen = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255]);
+BombGamedisplay = [7,7,7,7,7;7,7,7,7,7];
 drawScene(BombGameScreen,BombGamedisplay)
-
 Game = LevelOne;
 %%Logic of game
 while Game == LevelOne
+    r = randi(10);
+    figure (1)
     drawScene(BombGameScreen,BombGamedisplay)
     title('level one')
     xlabel('Pick one card')
     [x,y] = getMouseInput(BombGameScreen);
-    if x == 2 && y == 1
-        close all
-        fprintf('Have a good day!\n')
-        menurunning = false;
+    t = rand(1,10);
+    disp(t)
+    if x == r && y == 1
+        fprintf('You made it past level 1\n')
     else
         Game = LevelTwo;
     end
