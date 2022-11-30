44cfc98b532d516cc8cb7856a1f148e9f02aeffa
h2ogr <h2ogr@Matts-HP-Omen>
2022-11-30 12:15:58 -0500

    Maybe Adding a server?

--- GameMenu.m
+++ GameMenu.m
@@ -5,17 +5,20 @@ close all
 %% Outlining the intitial menu.
 %The menu is a 4x4 grid of buttons, with an exit, game 1, game 2, and game
 %3 button, game two and game three are both variation of the guessing game
-screen = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255])
-display = [7,5;4,3]
-drawScene(screen,display)
+fprintf('Welcome to the Q gaming engine! Let''s get started!\n')
+
+
+screen = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255]);
+display = [7,5;4,3];
+drawScene(screen,display);
 title('Q Games Launcher')
 
 
 % Make it search for mouse input and execute the games
-menurunning = true
+menurunning = true;
 
 while menurunning == true
-drawScene(screen,display)
+drawScene(screen,display);
 [x,y] = getMouseInput(screen);
 
 if x == 2 && y == 1
