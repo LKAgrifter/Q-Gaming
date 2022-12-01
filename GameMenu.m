1c952ca0b12a378511af43921b60c90d89ac750d
h2ogrifter <h2ogrifter@gmail.com>
2022-11-30 23:32:01 -0500

    Basically The entire guessing game for the computer guessing portion

--- GameMenu.m
+++ GameMenu.m
@@ -5,7 +5,7 @@ close all
 %% Outlining the intitial menu.
 %The menu is a 4x4 grid of buttons, with an exit, game 1, game 2, and game
 %3 button, game two and game three are both variation of the guessing game
-fprintf('Welcome to the Q gaming engine! Let''s get started!\n')
+fprintf('Welcome to the Q gaming engine! Let''s get started!\n\n')
 
 
 screen = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255]);
@@ -17,20 +17,20 @@ title('Q Games Launcher')
 % Make it search for mouse input and execute the games
 menurunning = true;
 
-while menurunning == true
+while menurunning == true;
 drawScene(screen,display);
 [x,y] = getMouseInput(screen);
 
 if x == 2 && y == 1;
 close all
-fprintf('Have a good day!\n')
+fprintf('\nHave a good day!\n')
 menurunning = false;
 close all
 end
 if x == 2 && y == 2;
-    fprintf('Initiating Guessing Game!\n')
+    fprintf('Initiating Guessing Game!\n\n')
     menurunning = false;
-    menurunning = guessinggame(1,2)
+    menurunning = guessinggame(1,2);
 end
 
 end
--- guessinggame.m
+++ guessinggame.m
@@ -8,30 +8,101 @@ close all
 %Explain the game
 
 input(['This game will display 7 cards. Pick a number between 1 and 100. \n' ...
-    'If your number is on the card shown, press y. \nIf your number is not on the card, press n \press enter to continue'])
+    'If your number is on the card shown, press y. \nIf your number is not on the card, press enter to continue \nIf you want to quit while the game is running, press q'])
 
+%Define card indexes
+cards = [1,2,3,4,5,6,7];
+cardspostion = [1,2,3,4,5,7,8];
+cardsvalue = [1,8,56,2,15,4,28];
 
-cards = [1,2,4,8,15,28,56];
-
-screenguess = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255]);
-display = [1];
+screenguess = simpleGameEngine('guessinggamecards.png',637,874,3,[255,255,255]);
+display = [9];
 drawScene(screenguess,display);
 title('Q Games Launcher');
 running = true;
 
-%making the running loop
+
+
+%% making the running loop
 while running == true;
 
-%always need keyboard input
-k = getKeyboardInput(screenguess);
+%Reset xlabel if looped
+xlabel('')
 
-%testing to see if the player wants to return to the main menu
-if length(k) == 1;
-    if k == 'q';
-        running = false;
-        menurun = true;
+%Set the number guesser value
+yournumber = 0;
+
+%Randomize the cards initially 
+cards_rand = cards(randperm(length(cards)));
+
+%show the cards
+for i = 1:1:length(cards);
+    i = cards_rand(i);
+    display = cardspostion(i);
+    drawScene(screenguess,display);
+    while true;
+        
+        %always need keyboard input
+        k = getKeyboardInput(screenguess);
+
+        %Check to see if the number is on the card
+        if length(k) == 1;
+            
+            %Add new number to your number if it's on the card, Then break
+            %the loop to load the next card
+            if k == 'y';
+                yournumber = yournumber + cardsvalue(i);
+                pause(0.5);
+                break
+
+            %Break the loop and allow it to load the next card     
+            elseif k == 'n';
+                answering = false;
+                pause(0.5);
+                break
+                
+                %If q is pressed, return to the main menu
+            elseif k == 'q';
+                    running = false;
+                    menurun = true;
+                    close all
+                    return
+            end
+        end
+    if running == false;
         break
     end
+    end
 end
-close all
+
+%Display Number at the end
+    if yournumber <=100 && yournumber>=1
+    fprintf("\nI bet your number was %.i!", yournumber)
+    fprintf('Press Space to play again! Press q to return to menu')
+    xlabel(['Number was ',num2str(yournumber),'! Press Space to play again! Press q to return to menu'])
+    
+    else
+    fprinf('I bet you lied to me! Your number was Not on one of those cards!!! Press Space to play again! Press q to return to menu')
+    xlabel('Lying is bad! Press Space to play again! Press q to return to menu.')
+    end
+
+%Prompt to play again
+
+while true;
+        k = getKeyboardInput(screenguess);
+        if length(k) == 1;
+            if k == 'q';
+                running = false;
+                menurun = true;
+                close all;
+                return
+            end
+        elseif length(k) == 5;
+            if k == 'space';
+                break
+            end
+        end
+end
+
+%End main While loop
 end
