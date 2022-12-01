4ad32efd35e9e8c9b0a4b6355ad49ee5d0159ffb
h2ogrifter <h2ogrifter@gmail.com>
2022-12-01 11:49:24 -0500

    BombGame/minesweeoper

---
+++ BombGame.m
@@ -0,0 +1,11 @@
+clc
+
+BombGameScreen = simpleGameEngine('MinesweeperSprite.png',16,16,5,[255,255,255]);
+BombGamedisplay = ones(10)
+
+
+%while BombGamedisplay==
+%[11:43 AM]
+%how would
+%[11:43 AM]
+%I replace some of the ones with mines?
