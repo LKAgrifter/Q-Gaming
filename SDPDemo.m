dd3dc50a386ce3c82dd49020a897919535b408c8
Jacks <Jacks@172.28.234.119>
2022-11-30 19:39:51 -0600

    further editing required

--- SDPDemo.m
+++ SDPDemo.m
@@ -8,22 +8,61 @@ close all
 BombGameScreen = simpleGameEngine('retro_cards.png',16,16,5,[255,255,255]);
 BombGamedisplay = [7,7,7,7,7;7,7,7,7,7];
 drawScene(BombGameScreen,BombGamedisplay)
-Game = LevelOne;
-%%Logic of game
-while Game == LevelOne
-    r = randi(10);
+Game = 1;
+%% Logic of game
+while Game == 1
     figure (1)
     drawScene(BombGameScreen,BombGamedisplay)
     title('level one')
     xlabel('Pick one card')
     [x,y] = getMouseInput(BombGameScreen);
-    t = rand(1,10);
-    disp(t)
-    if x == r && y == 1
-        fprintf('You made it past level 1\n')
+    r = randi(5);
+    disp(r)
+    if x == r && y == 1 || y == 2
+        fprintf('You made it to level 1\n')
+        gameover = 1;
     else
-        Game = LevelTwo;
+        Game = 2;
+        break   
     end
 end
-while Game == LevelTwo
+disp(x)
+disp(y)
+while Game == 2
+pause(2)
+    figure (1)
+    drawScene(BombGameScreen,BombGamedisplay)
+    title('level two')
+    xlabel('Pick one card')
+    [x,y] = getMouseInput(BombGameScreen);
+    r = randi(5);
+    e = randi(5);
+    disp(r)
+    if x == r || x == e && y == 1 || y == 2
+        fprintf('You made it to level 2\n')
+        gameover = 1;
+    else
+        Game = 3;
+        break   
+    end
 end
+while Game == 3
+pause(2)
+    figure (1)
+    drawScene(BombGameScreen,BombGamedisplay)
+    title('level three')
+    xlabel('Pick one card')
+    [x,y] = getMouseInput(BombGameScreen);
+    r = randi(5);
+    e = randi(5);
+    disp(r)
+    if x == r || x == e && y == 1 || y == 2
+        fprintf('You made it to level 3\n')
+        gameover = 1;
+    else
+        Game = 4;
+        break   
+    end
+end
+
+
