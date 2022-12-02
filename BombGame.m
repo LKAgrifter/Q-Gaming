d5644d58b82ab006f343c0b0608e4e2e572be9a9
h2ogrifter <h2ogrifter@gmail.com>
2022-12-01 19:57:56 -0500

    BombGame updates

--- BombGame.m
+++ BombGame.m
@@ -1,29 +1,48 @@
-function menurun = bombgame(username)
+function menurun = bombgame(username);
 
 clc
 close all
 
 %Define Screen
 BombGameScreen = simpleGameEngine('MinesweeperSprite.png',16,16,5,[255,255,255]);
-BombGamedisplay = ones(10)
-
-numberbombs = input("How many bombs would you like to have? ")
-
-%Lets place some bombs
-for i = 1:1:numberbombs
-    x = randi(10)
-    y = randi(10)
-    BombGamedisplay(y,x) = 2
-end
-drawScene(BombGameScreen,BombGamedisplay)
-
-numflags = 0 
 
 %Are we still playing?
 playing = true;
+ingame = true;
+wincounter = 0;
+
 while playing == true
+    
+    %making sure the gridsize is bigger than 0
+    gridsize = 0;
+    while gridsize <= 0
+        gridsize= input("How big do you want the grid to be? ");
+    end
+
+    %making sure the numberbombs is greater than 0
+    numberbombs = 0;
+    while numberbombs <= 0
+        numberbombs = input("How many bombs would you like to have? ");
+    end
+    
+    %Intitializing the early game
+    BombGamedisplay = ones(gridsize);
+
+    %Lets place some bombs
+    for i = 1:1:numberbombs
+        x = randi(gridsize)
+        y = randi(gridsize)
+        BombGamedisplay(y,x) = 2
+    end
+drawScene(BombGameScreen,BombGamedisplay)
+title('')
+
+numflags = 0 
+    
+    
+    
     %Loop to make the game portion of the game work
-    while true
+    while ingame == true
         %Define variables
         
         [x,y,z] = getMouseInput(BombGameScreen);
@@ -31,7 +50,8 @@ while playing == true
         %Let's uncover the bombs
         if BombGamedisplay(x,y) == 2 && z == 1
             BombGamedisplay(x,y) = 8
-            break
+            ingame = false
+            win = false
 
         elseif  BombGamedisplay(x,y) == 2 && z == 3
             if numflags < numberbombs
@@ -44,11 +64,11 @@ while playing == true
             spotcounter = 0
             %Checking around the spot
             for a = -1:1:1
-                d = x + a
+                a = x + a
                 for b = -1:1:1
-                    e = y + b
-                    if d < 11 && e < 11 && d > 0 && e > 0
-                        if BombGamedisplay(d,e) == 2 | BombGamedisplay(d,e) == 7
+                    b = y + b
+                    if a <= gridsize && b <= gridsize && a > 0 && b > 0
+                        if BombGamedisplay(a,b) == 2 | BombGamedisplay(a,b) == 7 | BombGamedisplay(a,b) == 8
                             spotcounter = spotcounter + 1
                             BombGamedisplay
                         end
@@ -80,10 +100,31 @@ while playing == true
 %                 return
 %             end
     end
-    % if numbombs = %sum(BombGamedisplay(:) == 7)
-    %They won!
-    drawScene(BombGameScreen,BombGamedisplay)
+    if numberbombs == sum(BombGamedisplay(:) == 7)
+        win = true
+        wincounter = wincounter + 1
     end
     drawScene(BombGameScreen,BombGamedisplay)
-    input('Play Again?')
+    
+    %If we 
+    if win == false
+        title('You Lost, Press Space to Play Again, or A to Return To Menu')
+        k = getKeyboardInput(BombGameScreen)
+        if length(k) == 1
+            if k == 'q'
+                close all
+                clc
+                return
+            end
+        elseif length(k) == 5
+            if k == 'space'
+                fprintf('Ok we''re playing again!')
+                ingame = true
+            end
+        end
+    
+    % End of small while loop
+    end
+
+    drawScene(BombGameScreen,BombGamedisplay)
 end
